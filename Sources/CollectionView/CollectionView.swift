a70f9ae419ef68f033ef73b3d9109f0907ce7f37
Dan Pourhadi <dan@pourhadi.com>
2020-01-06 17:19:21 -0600

    updates to layout and other tweak

--- Sources/CollectionView/CollectionView.swift
+++ Sources/CollectionView/CollectionView.swift
@@ -5,14 +5,8 @@
 //  Copyright © 2019 dan pourhadi. All rights reserved.
 //
 
-import SwiftUI
 import Combine
-
-private let AssociationManager = NSObject()
-
-fileprivate struct AssociatedKeys {
-    static var store: UInt8 = 0
-}
+import SwiftUI
 
 public extension CollectionView {
     func rowPadding(_ padding: EdgeInsets) -> Self {
@@ -38,61 +32,8 @@ public extension CollectionView {
 
 fileprivate let ScrollViewCoordinateSpaceKey = "ScrollViewCoordinateSpace"
 
-private struct LazyRowContainer<Content> : View where Content : View {
-    
-    @State var visible = false
-    
-    let content: (Bool) -> Content
-    let visibleRowsPublisher: AnyPublisher<[Int], Never>
-    let rowId: Int
-    
-    init(rowId: Int,
-         visibleRowsPublisher: AnyPublisher<[Int], Never>,
-         currentVisibleRows: [Int],
-         @ViewBuilder _ content: @escaping (Bool) -> Content) {
-        self.rowId = rowId
-        self.visibleRowsPublisher = visibleRowsPublisher
-        self.content = content
-        
-        self.visible = currentVisibleRows.contains(rowId)
-    }
-    
-    var body : some View {
-        return self.content(self.visible)
-            .onReceive(self.visibleRowsPublisher) { (visibleRows) in
-                let vis = visibleRows.contains(self.rowId)
-                
-                if vis != self.visible {
-                    self.visible = vis
-                }
-        }
-    }
-    
-}
-
-
-public struct AsynchronousView : View {
-    
-    @State var content: AnyView? = nil
-    
-    let contentFuture: AnyPublisher<AnyView?, Never>
-
-    public var body : some View {
-        Group {
-            if content != nil {
-                self.content!
-            }
-            
-            EmptyView()
-        }.onReceive(self.contentFuture) { (content) in
-            self.content = content
-        }
-    }
-}
-
-public struct CollectionView<Item, ItemContent>: View where ItemContent : View, Item : Identifiable & Equatable {
-    private struct Row<Content> : View where Content : View {
-        
+public struct CollectionView<Item, ItemContent>: View where ItemContent: View, Item: Identifiable & Equatable {
+    private struct Row<Content>: View where Content: View {
         @Binding var selectedItems: [Item]
         @Binding var selectionMode: Bool
         
@@ -106,12 +47,10 @@ public struct CollectionView<Item, ItemContent>: View where ItemContent : View, 
             self.content = content
         }
         
-        var body : some View {
+        var body: some View {
             self.content()
         }
-        
     }
-    
     
     public struct Layout {
         public var rowPadding: EdgeInsets
@@ -128,69 +67,6 @@ public struct CollectionView<Item, ItemContent>: View where ItemContent : View, 
             self.itemSpacing = itemSpacing
             self.rowHeight = rowHeight
         }
-    }
-    
-    
-    private class StateStore: ObservableObject {
-        
-        init() {
-            print("init it")
-        }
-        
-        var totalRows = 0
-        var mainMetrics: GeometryProxy?
-        
-        var mainFrame = CGRect.zero
-        
-        var parent: CollectionView?
-        var scrollViewFrame = CGRect.zero
-        var contentOffset = CGPoint.zero {
-            didSet {
-                guard let mainMetrics = mainMetrics else { return }
-                var visible = [Int]()
-                
-                let boundingFrame = CGRect(x: 0, y: -(mainMetrics.safeAreaInsets.top) - (mainMetrics.size.height / 2), width: mainMetrics.size.width, height: mainMetrics.size.height + (mainMetrics.size.height))
-                
-                
-                var currentY: CGFloat = 0
-                for x in 0..<self.totalRows {
-                    let height = self.height(for: x)
-                    let frame = CGRect(x: 0, y: currentY + contentOffset.y, width: mainMetrics.size.width, height: height)
-                    
-                    if frame.intersects(boundingFrame) {
-                        visible.append(x)
-                    }
-                    
-                    currentY += height
-                }
-                
-                print(visible.count)
-                self.visibleRows = visible
-            }
-        }
-        
-        var rowFrames = [Int: CGRect]()
-        
-        @Published var visibleRows = [Int]()
-        
-        var metrics = [GeometryProxy]()
-        
-        func height(for row: Int) -> CGFloat {
-            guard let parent = parent, let metrics = mainMetrics else {
-                return 0
-            }
-            
-            return parent.getRowHeight(for: row, metrics: metrics) + parent.layout.rowPadding.top + parent.layout.rowPadding.bottom
-        }
-    }
-    
-    @ObservedObject private var _store: StateStore
-    private var store:StateStore {
-        if _store.parent == nil {
-            _store.parent = self
-        }
-        
-        return _store
     }
     
     public typealias CollectionViewRowHeightBlock = (_ row: Int, _ rowMetrics: GeometryProxy, _ itemSpacing: CGFloat, _ numberOfColumns: Int) -> CGFloat
@@ -225,7 +101,6 @@ public struct CollectionView<Item, ItemContent>: View where ItemContent : View, 
     private let longPressAction: ((Item, GeometryProxy) -> Void)?
     private let pressAction: ((Item, Bool) -> Void)?
     
-
     public init(items: Binding<[Item]>,
                 selectedItems: Binding<[Item]>,
                 selectionMode: Binding<Bool>,
@@ -259,7 +134,6 @@ public struct CollectionView<Item, ItemContent>: View where ItemContent : View, 
             if currentRow.count >= self.numberOfColumns {
                 rows.append(ItemRow(id: rows.count, items: currentRow))
                 currentRow = []
-                
             }
         }
         
@@ -267,26 +141,20 @@ public struct CollectionView<Item, ItemContent>: View where ItemContent : View, 
             rows.append(ItemRow(id: rows.count, items: currentRow))
         }
         
-        self.store.totalRows = rows.count
-        
         return Group {
             GeometryReader { metrics in
                 ScrollView {
                     VStack(spacing: self.itemSpacing) {
                         ForEach(rows) { row in
-                            self.getRow(for: row, metrics: metrics, visible: true).padding(self.layout.rowPadding)
+                            self.getRow(for: row, metrics: metrics).padding(self.layout.rowPadding)
                         }
-                    }.onAppear(perform: {
-                        self.store.mainMetrics = metrics
-                        self.store.scrollViewFrame = metrics.frame(in: .global)
-                        print("appear")
-                    }).coordinateSpace(name: ScrollViewCoordinateSpaceKey)
+                    }.coordinateSpace(name: ScrollViewCoordinateSpaceKey)
                 }
             }
         }
     }
     
-    private func getRow(for row: ItemRow, metrics: GeometryProxy, visible: Bool) -> some View {
+    private func getRow(for row: ItemRow, metrics: GeometryProxy) -> some View {
         return Row(selectedItems: self.selectedItems, selectionMode: self.selectionMode) {
             HStack(spacing: self.itemSpacing) {
                 ForEach(row.items) { item in
@@ -304,26 +172,26 @@ public struct CollectionView<Item, ItemContent>: View where ItemContent : View, 
                                 }
                             }
                             .zIndex(2)
-                                .allowsHitTesting(false)
-
+                            .allowsHitTesting(false)
+                            
                             Group {
                                 Rectangle().foregroundColor(Color.clear)
                             }
                             .background(Color(UIColor.systemBackground))
-                                .allowsHitTesting(true)
-                                .zIndex(1)
-                                .onTapGesture {
-                                    if self.selectionMode.wrappedValue {
-                                        if let index = self.selectedItems.wrappedValue.firstIndex(of: item) {
-                                            self.selectedItems.wrappedValue.remove(at: index)
-                                        } else {
-                                            self.selectedItems.wrappedValue.append(item)
-                                        }
+                            .allowsHitTesting(true)
+                            .zIndex(1)
+                            .onTapGesture {
+                                if self.selectionMode.wrappedValue {
+                                    if let index = self.selectedItems.wrappedValue.firstIndex(of: item) {
+                                        self.selectedItems.wrappedValue.remove(at: index)
                                     } else {
-                                        self.selectedItems.wrappedValue = [item]
+                                        self.selectedItems.wrappedValue.append(item)
                                     }
-                                    
-                                    self.tapAction?(item, itemMetrics)
+                                } else {
+                                    self.selectedItems.wrappedValue = [item]
+                                }
+                                
+                                self.tapAction?(item, itemMetrics)
                             }
                             .onLongPressGesture(minimumDuration: 0.25, maximumDistance: 10, pressing: { pressing in
                                 self.pressAction?(item, pressing)
@@ -335,14 +203,11 @@ public struct CollectionView<Item, ItemContent>: View where ItemContent : View, 
                     }
                 }
             }.frame(height: self.getRowHeight(for: row.id, metrics: metrics))
-
-                
         }
-        
     }
     
     private func getColumnWidth(for width: CGFloat) -> CGFloat {
-        let w = (((width - (self.layout.rowPadding.leading + self.layout.rowPadding.trailing + (self.layout.itemSpacing * CGFloat(self.layout.numberOfColumns - 1)))) / CGFloat(self.layout.numberOfColumns)))
+        let w = ((width - (self.layout.rowPadding.leading + self.layout.rowPadding.trailing + (self.layout.itemSpacing * CGFloat(self.layout.numberOfColumns - 1)))) / CGFloat(self.layout.numberOfColumns))
         
         return w
     }
@@ -355,34 +220,32 @@ public struct CollectionView<Item, ItemContent>: View where ItemContent : View, 
         case .sameAsItemWidth:
             return self.getColumnWidth(for: metrics.size.width)
         case .dynamic(let rowHeightBlock):
-            return rowHeightBlock(row, metrics, itemSpacing, numberOfColumns)
+            return rowHeightBlock(row, metrics, self.itemSpacing, self.numberOfColumns)
         }
     }
 }
-
 
 struct CollectionView_Previews: PreviewProvider {
     struct ItemModel: Identifiable, Equatable {
         let id: Int
         let color: Color
     }
-
+    
     @State static var items = [ItemModel(id: 0, color: Color.red),
                                ItemModel(id: 1, color: Color.blue),
                                ItemModel(id: 2, color: Color.green),
                                ItemModel(id: 3, color: Color.yellow),
                                ItemModel(id: 4, color: Color.orange),
                                ItemModel(id: 5, color: Color.purple)]
-
-
+    
     @State static var selectedItems = [ItemModel]()
     @State static var selectionMode = false
-
+    
     static var previews: some View {
         CollectionView(items: $items,
                        selectedItems: $selectedItems,
                        selectionMode: $selectionMode)
-        { item, metrics in
+        { item, _ in
             Rectangle()
                 .foregroundColor(item.color)
         }
